3268ff4061623a18cf9a6f873344366a4909cecb
lee-gyoung <rock2me123@gmail.com>
2024-04-25 23:37:35 +0900

    웹 폰트 적용, header_menu 부분 수정 및 보완

--- 2024.04.25 헤더예제/구분해더/script/main.js
+++
@@ -1,6 +0,0 @@
-let btnCall = document.querySelector('.btnCall');
-let menuMo = document.querySelector('.menuMo');
-
-btnCall.addEventListener('click', (e) => {
-	menuMo.classList.toggle('on');
-});
