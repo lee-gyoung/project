aec470498e835dd017c20fb1c6df11ac9412d959
lee-gyoung <rock@me123@gmail.com>
2024-05-14 20:11:01 +0900

    페이드인아웃 슬라이드 완성, 베스트 아이템 섹션 제작중

--- script/main.js
+++ script/main.js
@@ -9,31 +9,11 @@ $('.header_slide').hover(
 );
 
 // 모바일 버전 서브 메뉴 클릭 이벤트
-//호버할때
-// $('.btn_fold').hover(
-// 	function () {
-// 		$(this).children('.btn_sub_menu').stop().slideDown();
-// 	},
-// 	function () {
-// 		$(this).children('.btn_sub_menu').stop().slideUp();
-// 	}
-// );
 
-//클릭할때 1번코드
 $('.btn_fold').click(function () {
 	let $sub = $(this).children('.btn_sub_menu');
 	$(this).children('.btn_sub_menu').stop().slideToggle();
 });
-
-//클릭할때 2번코드
-// $('.btn_fold').click(function () {
-// 	let $sub = $(this).children('.btn_sub_menu');
-// 	if ($sub.is(':visible')) {
-// 		$sub.stop().slideUp();
-// 	} else {
-// 		$sub.stop().slideDown();
-// 	}
-// });
 
 // 헤더 햄버거 버튼 작동
 let leftBtn = document.querySelector('#header_left_btn');
@@ -54,30 +34,19 @@ var swiper = new Swiper('#main_slide_left', {
 	centeredSlides: true,
 	loop: true,
 	autoplay: {
-		delay: 4000,
-		disableOnInteraction: true,
+		delay: 3600,
+		disableOnInteraction: false,
 	},
 });
 
 // 우측 슬라이드
-// var swipers = new Swiper('#main_slide_right', {
-// 	direction: 'vertical',
-// 	slidesPerView: 'auto',
-// 	// spaceBetween: 30,
-// 	mousewheel: true,
-// 	freeMode: true,
-// 	// pagination: {
-// 	// 	el: '.pagination-right',
-// 	// 	clickable: true,
-// 	// },
-// 	scrollbar: {
-// 		el: '.swiper-scrollbar',
-// 	},
-// });
+$('#right_slide_wrapper .right_slide_pics li:gt(0)').hide();
 
-// 해당 페이지 도달하면 스크롤 내려가게끔 수정 필요,
-// 마우스가 어디에 있든 슬라이드 움직일 수 있게하면 좋을듯
-// 스크롤 종료 시 다음 페이지로 넘어갈 수 있게
-
-// 스크롤 이벤트
-let slides = document.querySelectorAll('.main_slide');
+setInterval(function () {
+	$('#right_slide_wrapper .right_slide_pics li:first-child')
+		.fadeOut()
+		.next('li')
+		.fadeIn()
+		.end()
+		.appendTo('.right_slide_pics');
+}, 3900);
