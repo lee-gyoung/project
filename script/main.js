7b4a4edf7b2c6c49e4eda7b940a51caa74a9d21c
lee-gyoung <rock@me123@gmail.com>
2024-05-21 21:03:02 +0900

    초기화면 스크롤 버튼 자바스크립트 완성, 푸터 부분 대략적인 틀 생성, 전체적인 마크업 재검토, Contact Slide에 쓸 소스 파일 보정 중

--- script/main.js
+++ script/main.js
@@ -9,7 +9,6 @@ $('.header_slide').hover(
 );
 
 // 모바일 버전 서브 메뉴 클릭 이벤트
-
 $('.btn_fold').click(function () {
 	let $sub = $(this).children('.btn_sub_menu');
 	$(this).children('.btn_sub_menu').stop().slideToggle();
@@ -23,6 +22,21 @@ leftBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 	leftBtn.classList.toggle('on');
 	leftMenu.classList.toggle('on');
+});
+
+// 초기화면 스크롤 버튼 작동
+// 1.
+// $(document).ready(function ($) {
+// 	$('#scrBtn').click(function (e) {
+// 		e.preventDefault();
+// 		$('html').animate({ scrollTop: $('#scrBtn').offset().top }, 500);
+// 	});
+// });
+
+// 2.
+let scrBtn = document.querySelector('#scrBtn');
+scrBtn.addEventListener('click', function () {
+	window.scrollTo({ left: 0, top: 860, behavior: 'smooth' });
 });
 
 // 좌측 슬라이드
@@ -50,3 +64,20 @@ setInterval(function () {
 		.end()
 		.appendTo('.right_slide_pics');
 }, 3900);
+
+// 비주얼 파트 로고 교체 이벤트
+// let green = url('../img/visual part/2green.png');
+// let pink = url('../img/visual part/3pink.png');
+// let blue = url('../img/visual part/4blue.png');
+
+// $('.click').click(function() {
+// 	$('img.class').fadeOut(300, function(){
+// 	   $(this).attr('src','new_src.png').bind('onreadystatechange load', function(){
+// 		  if (this.complete) $(this).fadeIn(300);
+// 	   });
+// 	});
+//  });
+
+window.on('scroll', function () {});
+$('.main_visual');
+$('#visual_logo').attr({ src: '../img/visual part/2green.png' });
