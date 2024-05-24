b3ee11f768404768dc3dcf9c2543c6e5247664e2
lee-gyoung <rock@me123@gmail.com>
2024-05-24 21:16:37 +0900

    메인페이지 탭메뉴 제작중, 푸터 1차  완성

---
+++ script/jqurey_function.js
@@ -0,0 +1,26 @@
+// 헤더 서브 메뉴 슬라이드
+$('.header_slide').hover(
+	function () {
+		$(this).children('li > .sub_menu').stop().slideDown();
+	},
+	function () {
+		$(this).children('li > .sub_menu').stop().slideUp();
+	}
+);
+
+// 모바일 버전 서브 메뉴 클릭 이벤트
+$('.btn_fold').click(function () {
+	$(this).children('.btn_sub_menu').stop().slideToggle();
+});
+
+// 우측 슬라이드
+$('#right_slide_wrapper .right_slide_pics li:gt(0)').hide();
+
+setInterval(function () {
+	$('#right_slide_wrapper .right_slide_pics li:first-child')
+		.fadeOut()
+		.next('li')
+		.fadeIn()
+		.end()
+		.appendTo('.right_slide_pics');
+}, 3900);
--- script/main.js
+++ script/main.js
@@ -1,19 +1,3 @@
-// 헤더 서브 메뉴 슬라이드
-$('.header_slide').hover(
-	function () {
-		$(this).children('li > .sub_menu').stop().slideDown();
-	},
-	function () {
-		$(this).children('li > .sub_menu').stop().slideUp();
-	}
-);
-
-// 모바일 버전 서브 메뉴 클릭 이벤트
-$('.btn_fold').click(function () {
-	let $sub = $(this).children('.btn_sub_menu');
-	$(this).children('.btn_sub_menu').stop().slideToggle();
-});
-
 // 헤더 햄버거 버튼 작동
 let leftBtn = document.querySelector('#header_left_btn');
 let leftMenu = document.querySelector('#left_btn_menu');
@@ -25,18 +9,9 @@ leftBtn.addEventListener('click', (e) => {
 });
 
 // 초기화면 스크롤 버튼 작동
-// 1.
-// $(document).ready(function ($) {
-// 	$('#scrBtn').click(function (e) {
-// 		e.preventDefault();
-// 		$('html').animate({ scrollTop: $('#scrBtn').offset().top }, 500);
-// 	});
-// });
-
-// 2.
 let scrBtn = document.querySelector('#scrBtn');
 scrBtn.addEventListener('click', function () {
-	window.scrollTo({ left: 0, top: 860, behavior: 'smooth' });
+	window.scrollTo({ left: 0, top: 960, behavior: 'smooth' });
 });
 
 // 좌측 슬라이드
@@ -53,67 +28,25 @@ var swiper = new Swiper('#main_slide_left', {
 	},
 });
 
-// 우측 슬라이드
-$('#right_slide_wrapper .right_slide_pics li:gt(0)').hide();
-
-setInterval(function () {
-	$('#right_slide_wrapper .right_slide_pics li:first-child')
-		.fadeOut()
-		.next('li')
-		.fadeIn()
-		.end()
-		.appendTo('.right_slide_pics');
-}, 3900);
-
-// 비주얼 파트 로고 교체 이벤트
-// $(document).ready(function () {
-// 	$(window).scroll(function () {
-// 		$('#visual_logo').each(function (i) {
-// 			var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 1;
-// 			var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-// 			if (bottom_of_window > bottom_of_element) {
-// 				$(this).animate({ opacity: '1' }, 1000);
-// 			}
-// 		});
-// 	});
-// });
-
-// $('#visual_logo').attr({ src: '../img/visual part/2green.png' });
-
-$(window).scroll(function () {
-	var height = $(window).scrollTop();
-	// console.log(height);
-	// if (height > 2500) {
-	// 	let vi = document.querySelector('#visual_logo');
-	// 	vi.setAttribute('src', './img/visual part/2green.png');
-	// } else {
-	// 	let vi = document.querySelector('#visual_logo');
-	// 	vi.setAttribute('src', './img/visual part/1w.png');
-	// 	console.log(height);
-	// }
-});
-
 // 베스트 아이템
 
 let sections = document.querySelectorAll('main .scroll_event');
 let sections_array = Array.from(sections);
-console.log(sections_array);
-console.log(sections[1].offsetTop);
-console.log(sections[2].offsetTop);
 
 let position_array = [];
 for (let el of sections_array) {
 	position_array.push(el.offsetTop);
 }
-
+let best_item_active_scrollTop = 1450;
 window.addEventListener('scroll', () => {
 	let scroll =
 		window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
 
 	sections_array.map((el, index) => {
-		if (scroll >= 1450) {
+		if (scroll >= best_item_active_scrollTop) {
 			sections_array[index].classList.add('on');
 		}
 	});
 });
+
+// 탭 섹션 호버 시 이미지 경로 변환
