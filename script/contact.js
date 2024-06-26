5bb58cc4c554a45761690509ffafe3371bdde453
lee-gyoung <rock2me123@gmail.com>
2024-06-26 18:07:21 +0900

    서브페이지 contact 완성

--- script/contact.js
+++ script/contact.js
@@ -1,10 +1,13 @@
-// 3fb677db211075e44976a3ab3ae0cefd
-
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var container = document.getElementById('map');
 var options = {
-	//지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-	level: 3, //지도의 레벨(확대, 축소 정도)
+	center: new kakao.maps.LatLng(37.5482585, 126.9189736),
+	level: 3,
 };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+var map = new kakao.maps.Map(container, options);
+var markerPosition = new kakao.maps.LatLng(37.5482585, 126.9189736);
+var marker = new kakao.maps.Marker({
+	position: markerPosition,
+});
+
+marker.setMap(map);
